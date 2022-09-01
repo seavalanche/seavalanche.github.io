f2c83322584e7719e529f8a44ffc61f96cab9353
seavalanche <seavalanche@gmail.com>
2022-09-02 02:00:50 +0700

    all failed

--- service-worker.js
+++ service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
   "/precache-manifest.9fa006a88f7eb1fb009c488c8cec6d13.js"
-=======
-  "/precache-manifest.e5ab62bb8a422168d74cf3884f64ee9e.js"
->>>>>>> parent of 131dd60 (didn't work)
 );
 
 workbox.clientsClaim();
