447d2a77459c9977b6685d6564a20036f4b4ad7f
seavalanche <seavalanche@gmail.com>
2022-09-02 02:13:19 +0700

    Revert "Updated router"
    This reverts commit 6f8bb8414b75569571feca47616d82661a74aa63.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87952249093c29be9e32dbe8bb8e21d2.js"
+  "/precache-manifest.ab306b1b22d3e516c299ba6aba8deeba.js"
 );
 
 workbox.clientsClaim();
