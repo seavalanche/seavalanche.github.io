6768369e9fec9be1b96728a64920f5404dd22635
vesnea <104858535+seavalanche@users.noreply.github.com>
2026-09-01 17:19:34 +0800

    Some minor adjustments

--- src/Components/Sparkles.js
+++ src/Components/Sparkles.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import '../styles/components/Sparkles.css';
 
 // The particle blueprint
